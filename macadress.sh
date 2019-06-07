5151a7e63e6dacd2b545a1b65895a1ca38e8c477
anurajvijayan <31343243+anurajvijayan@users.noreply.github.com>
2019-06-07 15:30:40 +0530

    Update macadress.sh

--- macadress.sh
+++ macadress.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+#Its query the "https://macaddress.io" MAC address lookup API
 curl -i -H "Accept: application/json" -H "Content-Type: application/json" https://api.macaddress.io/v1?apiKey=YOUR_API_KEY&output=json&search=43:38:39:ff:ef:21
